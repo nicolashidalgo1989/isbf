c96e895a4a712c3e35badf7185ea1d999d5e29a5
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2020-06-30 09:47:48 -0400

    Initial build  subtree commit

---
+++ build/precache-manifest.78a31d0a4dd271d0a1ff3f3037935f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa99dfbf28420d48353d619a86f0b6f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d747af9265e145ecf6b",
+    "url": "/static/css/main.48905785.chunk.css"
+  },
+  {
+    "revision": "abb1d8e7fdd579d58728",
+    "url": "/static/js/2.faf07eaa.chunk.js"
+  },
+  {
+    "revision": "acff744c5c5e21e2b14a11e29451820a",
+    "url": "/static/js/2.faf07eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d747af9265e145ecf6b",
+    "url": "/static/js/main.dfe841d3.chunk.js"
+  },
+  {
+    "revision": "be2bf6491cf1f5601e61",
+    "url": "/static/js/runtime-main.0e2f3d12.js"
+  }
+]);
